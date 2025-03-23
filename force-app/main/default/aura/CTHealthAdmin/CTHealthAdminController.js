fcd9cf5dbbcfc4448c2030a6acce1af3d9561e6f
Ty Adams <tyrelleadams28@gmail.com>
2025-03-23 13:48:57 -0700

    Add tab navigation to CTHealthAdmin component with Person and Location views

--- force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
+++ force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
@@ -6,5 +6,12 @@
         // Add functionality to handle the button click
         console.log('Add New button clicked');
         // You can add more functionality here, such as opening a modal or navigating to a record creation page
+    },
+    handleTabChange : function(component, event, helper) {
+        // Get tab ID from the event
+        var selectedTabId = event.getParam("id");
+        // Update the attribute
+        component.set("v.selectedTabId", selectedTabId);
+        console.log('Tab changed to: ' + selectedTabId);
     }
 })
