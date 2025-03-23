23b22e0ba527187ab08fa2bf87c0e748fe60941d
Ty Adams <tyrelleadams28@gmail.com>
2025-03-23 13:40:37 -0700

    Add "Add New" button to CTHealthAdmin component and implement click handler

--- force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
+++ force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
@@ -1,5 +1,10 @@
 ({
     myAction : function(component, event, helper) {
 
+    },
+    handleAddNew : function(component, event, helper) {
+        // Add functionality to handle the button click
+        console.log('Add New button clicked');
+        // You can add more functionality here, such as opening a modal or navigating to a record creation page
     }
 })
